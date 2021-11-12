884861dcb12b9279a3caf0220a93028c285f4e09
GUILHERME LEDIER ALVES <21192997@CFP127ALUNO.LOCAL>
2021-11-12 08:41:46 -0300

    add data base

---
+++ database/banco_dados.sql
@@ -0,0 +1,26 @@
+#CRIAR O BANCO DE DADOS:
+create database icadastro;
+
+#HABILITAR O BANCO DE DADOS:
+use icadastro;
+
+#CRIAR A TABELA DE PESSOAS NO BANCO DE DADOS:
+create table tbl_pessoa(
+cod_pessoa int unsigned auto_increment primary key,
+nome varchar(250) not null,
+sobrenome varchar(500) not null,
+email varchar(500) not null,
+celular varchar(20) not null
+);
+
+#CRIAR A TABELA DE ADMINISTRADORES NO BANCO DE DADOS:
+create table tblAdministrador(
+idAdministrador int unsigned auto_increment primary key,
+nome varchar(250) not null,
+sobrenome varchar(500) not null,
+email varchar(500) not null,
+celular varchar(20) not null,
+usuario varchar(255),
+senha varchar (255),
+unique index(idAdministrador, usuario)
+);
